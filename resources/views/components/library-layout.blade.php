40bcdccaa18aa550d7092f9320ccebecd6ed7d47
slightlightss66-stack <slightlightss66@gmail.com>
2026-04-15 16:12:07 +0300

    feat: application page  and csrf token

--- resources/views/components/library-layout.blade.php
+++ resources/views/components/library-layout.blade.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{env('APP_NAME')}}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {

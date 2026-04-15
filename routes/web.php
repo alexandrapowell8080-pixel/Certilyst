e7e6e10f2fa2ee61aeb503aba09a090768cf28ed
freelance41000 <freelance4920@gmail.com>
2026-04-15 14:06:54 +0300

    feat: Flashcard styles

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/flashcards', function () {
+    return view('flashcards.view');
+});
+

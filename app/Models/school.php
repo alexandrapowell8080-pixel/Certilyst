9e75f9da67fd6a42b40c4b629314ab9ea0dc8ae4
slightlightss66-stack <slightlightss66@gmail.com>
2026-04-15 17:08:58 +0300

    feat: relations

--- app/Models/school.php
+++ app/Models/school.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class school extends Model
 {
     //
     protected $fillable = ['name', 'slug'];
+
+    public function course():HasMany
+    {
+        return $this->hasMany(course::class,'school_id');
+    }
 }

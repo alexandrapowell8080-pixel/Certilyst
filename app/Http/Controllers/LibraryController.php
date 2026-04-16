6a8fd8abf8ebeac771956424959e927bb74c36b2
slightlightss66-stack <slightlightss66@gmail.com>
2026-04-16 14:48:32 +0300

    feat: fetch exam questions with the related flashcards

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exam;
 use App\Models\school;
+use App\Models\Subject;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -148,17 +150,29 @@ class LibraryController extends Controller
     public function index(): View
     {
         $schools = school::with('course.subject.exam')->get();
+
         return view('library.index', compact('schools'));
     }
 
+
+    /**
+     * Question with the related flashcard
+     *
+     * @param string $school
+     * @param string $course
+     * @param string $exam
+     * @return View
+     */
     public function questions(string $school, string $course, string $exam): View
     {
         $questions = $this->questions;
         $questions_count = count($questions);
         $question = $questions[0];
 
-        // Map the course to the subject slug for the Flashcards button
-        $subject_slug = $course;
+        // Flashcard by exam
+        $currentExam = Exam::with('subject')->where('slug', $exam)->firstOrFail();
+     
+        $subject_slug = $currentExam->subject->slug;
 
         return view('library.exam.questions', compact('question', 'questions_count', 'subject_slug', 'school'));
     }
@@ -191,8 +205,9 @@ class LibraryController extends Controller
         ]);
     }
 
-    public function nextQuestion(int $questionId){
-        
+    public function nextQuestion(int $questionId)
+    {
+
         $nextId = $questionId + 1;
 
         foreach ($this->questions as $question) {

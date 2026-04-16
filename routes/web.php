14b5c014b8d490dc9c2519991381d00da1a082a9
freelance41000 <freelance4920@gmail.com>
2026-04-16 09:19:30 +0300

    feat: Changend the flashcard routes

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,15 @@
 
 use App\Http\Controllers\LibraryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Flashcards\FlashcardsController;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/flashcards', function () {
-    return view('flashcards.view');
-});
+// Flash Card Routes
+Route::get('/flashcards',[FlashcardsController::class,'index'])->name('flashcards');
 
-
+// Library Routes
 Route::get('/library',[LibraryController::class,'index'])->name('library');
 Route::get('/real-estate/sales-person/illinois/version',[LibraryController::class,'questions'])->name('exam-questions');

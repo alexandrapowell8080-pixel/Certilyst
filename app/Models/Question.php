fbacf142ddaaf3cb604d8d33bd29e3cf75ef7840
slightlightss66-stack <slightlightss66@gmail.com>
2026-04-17 12:24:41 +0300

    feat: relation to exams

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Question extends Model
 {
-    //
+    public function exam():BelongsTo
+    {
+        return $this->belongsTo(Exam::class);
+    }
 }

3f2bc6d9db83ca9e7f0257e4032442d95eeeffac
freelance41000 <freelance4920@gmail.com>
2026-06-02 09:59:41 +0300

    Flashcard

--- app/Http/Controllers/Flashcards/FlashcardsController.php
+++ app/Http/Controllers/Flashcards/FlashcardsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Flashcards;
 
 use App\Http\Controllers\Controller;
+use App\Models\Flashcard;
 use App\Models\Subject;
 
 class FlashcardsController extends Controller
